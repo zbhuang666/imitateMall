1097d3f3a7db1f5bd803d193fa4f377c4cd3a14f
zbhuang <2630977504@qq.com>
2019-12-12 22:53:15 +0800

    添加父子依赖

---
+++ imitate-mall-sso/src/main/java/com/travelsky/mall/imitatemallsso/ImitateMallSsoApplication.java
@@ -0,0 +1,13 @@
+package com.travelsky.mall.imitatemallsso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImitateMallSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImitateMallSsoApplication.class, args);
+    }
+
+}
---
+++ imitate-mall-sso/src/test/java/com/travelsky/mall/imitatemallsso/ImitateMallSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelsky.mall.imitatemallsso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImitateMallSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
